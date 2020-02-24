f8b67a6a8ba08a1f7e1003cb8deca5070be70310
yukikaze506 <yukikaze506@gmail.com>
2020-02-24 04:42:02 +0000

    Youtube Crawlerのベース追加

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -2,4 +2,4 @@
 curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get update
-npm install -g prettier prettier-plugin-java typescript ts-node
+npm install -g prettier@1.19.1 typescript@3.8.2 ts-node@8.6.2 nodemon@2.0.2 tsconfig-paths@3.9.0

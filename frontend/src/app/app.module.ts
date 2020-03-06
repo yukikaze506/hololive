e9ede92928fd9666623eca11ce83d59b86cc2859
yukikaze506 <yukikaze506@gmail.com>
2020-03-06 06:10:55 +0000

    PWAファイルの追加

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,6 +2,9 @@ import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpInterceptorService } from './services/interceptor/http-interceptor.service';
@@ -9,7 +12,14 @@ import { SharedModule } from './shared/shared.module';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, SharedModule],
+    imports: [
+        BrowserModule.withServerTransition({ appId: 'serverApp' }),
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        SharedModule,
+        ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+        RouterModule
+    ],
     providers: [[{ provide: HTTP_INTERCEPTORS, useClass: HttpInterceptorService, multi: true }]],
     bootstrap: [AppComponent]
 })
---
+++ frontend/src/app/app.server.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { ServerModule } from '@angular/platform-server';
+import { RouterModule, Routes } from '@angular/router';
+import { AppShellComponent } from './app-shell/app-shell.component';
+import { AppComponent } from './app.component';
+import { AppModule } from './app.module';
+
+const routes: Routes = [{ path: 'hololive', component: AppShellComponent }];
+
+@NgModule({
+    imports: [AppModule, ServerModule, RouterModule.forRoot(routes)],
+    bootstrap: [AppComponent],
+    declarations: [AppShellComponent]
+})
+export class AppServerModule {}
